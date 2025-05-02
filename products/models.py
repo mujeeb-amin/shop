0ef51ad2ceb2cd4372828d17ea3ad45c5ffddab8
Mujeeb Rahman Amin <mujeebrahmanamin2@gmail.com>
2025-05-02 22:13:42 +0430

    Feat: create category and product models.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=120, )
+    price = models.FloatField(default=10)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+
+

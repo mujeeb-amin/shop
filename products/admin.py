d97fe063f6c41bf715d0d1eb8c2237d96ec864b2
Mujeeb Rahman Amin <mujeebrahmanamin2@gmail.com>
2025-05-25 20:55:04 +0430

    Feat: add admin config for product.

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 # Register your models here.
 from products.models import Category, Product
 
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ["title", "price","type", "category", "updated_date", "is_published"]
+    list_display_links = ["title", 'price']
+    list_editable = ["is_published"]
+    list_filter = ["category", "is_published"]
+    search_fields = ["title", "price"]
+    readonly_fields = ["created_date", "updated_date"]
+    # list_per_page = 2
+    fields = ["title", "price","type", "category", "is_published", "created_date", "updated_date"]
+    
+
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -12,6 +12,12 @@ class Product(models.Model):
     title = models.CharField(max_length=120, )
     price = models.FloatField(default=10)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    type = models.CharField(choices=[("for_kids", "For Kids"), 
+                                     ("for_men", "For Men"), ("for_women", "For women")], 
+                                     max_length=120, blank=True, null=True)
+    is_published = models.BooleanField(default=True)
 
 
     def __str__(self):

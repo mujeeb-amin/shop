0fe03b528e56cb4373ce4d89c4fbb2ca1a7ed95f
Mujeeb Rahman Amin <mujeebrahmanamin2@gmail.com>
2025-06-20 22:02:08 +0430

    Feat: add get by id views

--- products/urls.py
+++ products/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from dashboard.views import home
+from products.views import get_products, get_product
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", home),
-    path("products/", include("products.urls")),
+    path("", get_products),  # /products/
+    path("<int:product_id>/", get_product),  # /products/4
 ]
--- products/views.py
+++ products/views.py
@@ -6,3 +6,8 @@ from products.models import Product
 def get_products(request: WSGIRequest):
     products = Product.objects.all()
     return render(request, "products/list.html", {"products": products})
+
+
+def get_product(request: WSGIRequest, product_id: int):
+    product = Product.objects.get(id=product_id)
+    return render(request, "products/detail.html", {"product": product})

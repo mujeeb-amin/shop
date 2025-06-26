1ae2d1fdb495d7e28c0d8341fbddf5a9729ae9ff
Mujeeb Rahman Amin <mujeebrahmanamin2@gmail.com>
2025-06-26 19:45:06 +0430

    Feat: update and deleted product added.

--- products/urls.py
+++ products/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 
 from django.urls import path
 
-from products.views import get_products, get_product, create_product
+from products.views import (
+    get_products,
+    get_product,
+    create_product,
+    update_product,
+    delete_product,
+)
 
 app_name = "products"
 
@@ -25,4 +31,10 @@ urlpatterns = [
     path("", get_products, name="product_list"),  # /products/
     path("<int:product_id>/", get_product, name="product_get"),  # /products/4
     path("create/", create_product, name="product_create"),  # /products/
+    path(
+        "update/<int:product_id>/", update_product, name="product_update"
+    ),  # /products/
+    path(
+        "delete/<int:product_id>/", delete_product, name="product_delete"
+    ),  # /products/
 ]
--- products/views.py
+++ products/views.py
@@ -36,3 +36,38 @@ def create_product(request: WSGIRequest):
     )
 
     return redirect(reverse("products:product_list"))
+
+
+def update_product(request: WSGIRequest, product_id):
+    if request.method == "GET":
+        categories = Category.objects.all()
+        product = Product.objects.get(id=product_id)
+        return render(
+            request,
+            "products/update.html",
+            {"categories": categories, "product": product},
+        )
+
+    title = request.POST.get("title")
+    price = request.POST.get("price")
+    is_published = request.POST.get("is_published")
+    type = request.POST.get("type")
+    category_id = request.POST.get("category")
+
+    category = Category.objects.get(id=category_id)
+
+    Product.objects.filter(id=product_id).update(
+        title=title,
+        price=price,
+        is_published=True if is_published == "on" else False,
+        type=type,
+        category=category,
+    )
+
+    return redirect(reverse("products:product_get", kwargs={"product_id": product_id}))
+
+
+def delete_product(request: WSGIRequest, product_id: int):
+    product = Product.objects.get(id=product_id)
+    product.delete()
+    return redirect(reverse("products:product_list"))
